ac72f5cef33030abaf1ae1843900dffa211bb2f1
guowei-gong <gongguowei01@gmail.com>
2024-09-25 15:06:56 +0800

    feat(log): 添加对外暴露的日志打印函数

--- logger.go
+++ logger.go
@@ -8,6 +8,8 @@ import (
 	"os"
 )
 
+var globalLogger *Logger
+
 type Logger struct {
 	logger *zap.SugaredLogger
 	opts   *options
@@ -16,6 +18,7 @@ type Logger struct {
 var levelMap map[zapcore.Level]Level
 
 func init() {
+
 	levelMap = map[zapcore.Level]Level{
 		zap.DebugLevel:  DebugLevel,
 		zap.InfoLevel:   InfoLevel,
@@ -24,6 +27,20 @@ func init() {
 		zap.FatalLevel:  FatalLevel,
 		zap.DPanicLevel: PanicLevel,
 	}
+
+	SetLogger(NewLogger(WithStackLevel(WarnLevel),
+		WithFormat(TextFormat),
+		WithCallerSkip(2),
+		WithClassifiedStorage(true)))
+}
+
+// SetLogger 设置日志记录器
+func SetLogger(logger *Logger) {
+	if logger == nil {
+		return
+	}
+
+	globalLogger = logger
 }
 
 func NewLogger(opts ...Option) *Logger {
@@ -154,6 +171,104 @@ func (l *Logger) print(level Level, stack bool, a ...interface{}) {
 		l.logger.Fatalw(msg, encoder.StackFlag, stack)
 	case PanicLevel:
 		l.logger.DPanicw(msg, encoder.StackFlag, stack)
+	}
+}
+
+// Print 打印日志，不含堆栈信息
+func Print(level Level, a ...interface{}) {
+	if globalLogger != nil {
+		globalLogger.Print(level, a...)
+	}
+}
+
+// Printf 打印模板日志，不含堆栈信息
+func Printf(level Level, format string, a ...interface{}) {
+	if globalLogger != nil {
+		globalLogger.Printf(level, format, a...)
+	}
+}
+
+// Debug 打印调试日志
+func Debug(a ...interface{}) {
+	if globalLogger != nil {
+		globalLogger.Debug(a...)
+	}
+}
+
+// Debugf 打印调试模板日志
+func Debugf(format string, a ...interface{}) {
+	if globalLogger != nil {
+		globalLogger.Debugf(format, a...)
+	}
+}
+
+// Info 打印信息日志
+func Info(a ...interface{}) {
+	if globalLogger != nil {
+		globalLogger.Info(a...)
+	}
+}
+
+// Infof 打印信息模板日志
+func Infof(format string, a ...interface{}) {
+	if globalLogger != nil {
+		globalLogger.Infof(format, a...)
+	}
+}
+
+// Warn 打印警告日志
+func Warn(a ...interface{}) {
+	if globalLogger != nil {
+		globalLogger.Warn(a...)
+	}
+}
+
+// Warnf 打印警告模板日志
+func Warnf(format string, a ...interface{}) {
+	if globalLogger != nil {
+		globalLogger.Warnf(format, a...)
+	}
+}
+
+// Error 打印错误日志
+func Error(a ...interface{}) {
+	if globalLogger != nil {
+		globalLogger.Error(a...)
+	}
+}
+
+// Errorf 打印错误模板日志
+func Errorf(format string, a ...interface{}) {
+	if globalLogger != nil {
+		globalLogger.Errorf(format, a...)
+	}
+}
+
+// Fatal 打印致命错误日志
+func Fatal(a ...interface{}) {
+	if globalLogger != nil {
+		globalLogger.Fatal(a...)
+	}
+}
+
+// Fatalf 打印致命错误模板日志
+func Fatalf(format string, a ...interface{}) {
+	if globalLogger != nil {
+		globalLogger.Fatalf(format, a...)
+	}
+}
+
+// Panic 打印Panic日志
+func Panic(a ...interface{}) {
+	if globalLogger != nil {
+		globalLogger.Panic(a...)
+	}
+}
+
+// Panicf 打印Panic模板日志
+func Panicf(format string, a ...interface{}) {
+	if globalLogger != nil {
+		globalLogger.Panicf(format, a...)
 	}
 }
 
