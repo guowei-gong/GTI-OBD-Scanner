53b13066ea3fee58e62a1f45bd4b4343a393944e
guowei-gong <gongguowei01@gmail.com>
2024-09-25 12:32:34 +0800

    feat(options): 选项模式设置日志初始化配置

--- options.go
+++ options.go
@@ -51,3 +51,65 @@ func defaultOptions() *options {
 	// 读取项目配置
 	return opts
 }
+
+// WithFile 设置输出的文件路径
+func WithFile(file string) Option {
+	return func(o *options) { o.file = file }
+}
+
+// WithLevel 设置输出的最低日志级别
+func WithLevel(level Level) Option {
+	return func(o *options) { o.level = level }
+}
+
+// WithFormat 设置输出的日志格式
+func WithFormat(format Format) Option {
+	return func(o *options) { o.format = format }
+}
+
+// WithStdout 设置是否输出到终端
+func WithStdout(enable bool) Option {
+	return func(o *options) { o.stdout = enable }
+}
+
+// WithTimeFormat 设置时间格式
+func WithTimeFormat(format string) Option {
+	return func(o *options) { o.timeFormat = format }
+}
+
+// WithStackLevel 设置堆栈的最小输出级别
+func WithStackLevel(level Level) Option {
+	return func(o *options) { o.stackLevel = level }
+}
+
+// WithFileMaxAge 设置文件最大留存时间
+func WithFileMaxAge(maxAge time.Duration) Option {
+	return func(o *options) { o.fileMaxAge = maxAge }
+}
+
+// WithFileMaxSize 设置输出的单个文件尺寸限制
+func WithFileMaxSize(size int64) Option {
+	return func(o *options) { o.fileMaxSize = size }
+}
+
+// WithFileCutRule 设置文件切割规则
+func WithFileCutRule(cutRule CutRule) Option {
+	return func(o *options) { o.fileCutRule = cutRule }
+}
+
+// WithCallerSkip 设置调用者跳过的层级深度
+func WithCallerSkip(skip int) Option {
+	return func(o *options) { o.callerSkip = skip }
+}
+
+// WithCallerFullPath 设置是否启用调用文件全路径
+func WithCallerFullPath(enable bool) Option {
+	return func(o *options) { o.callerFullPath = enable }
+}
+
+// WithClassifiedStorage 设置启用文件分级存储
+// 启用后，日志将进行分级存储，大一级的日志将存储于小于等于自身的日志级别文件中
+// 例如：InfoLevel级的日志将存储于due.debug.20220910.log、due.info.20220910.log两个日志文件中
+func WithClassifiedStorage(enable bool) Option {
+	return func(o *options) { o.classifiedStorage = enable }
+}
